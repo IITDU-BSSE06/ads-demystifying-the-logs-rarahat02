0a505b693bf80c0eaeac7c4e6d9d890d966003ad
Md. Ruhul Amin Rahat <rarahat02@gmail.com>
2017-11-07 20:53:09 +0600

    mapper file content uploaded for assignment 2

---
+++ assignment2/mapper.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+
+for line in sys.stdin:
+	eachData = line.strip().split(" - - ")
+	if len(eachData) == 2:
+		ip, rest = eachData
+		print "{0}\t{1}".format(ip, restOfData)
